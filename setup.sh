1a750b1b08641c03b89ced8545608d011e0e5b33
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2022-12-22 21:34:22 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,6 +28,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 
 #source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
 yay -S nvm
 
 source /usr/share/nvm/init-nvm.sh
