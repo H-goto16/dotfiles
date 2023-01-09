318941793598c5a746b6cdf51e46a7de09c3d9f3
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2023-01-09 11:33:19 +0900

    feat: minimal ver

--- setup.sh
+++ setup.sh
@@ -19,21 +19,6 @@ yay -S google-chrome
 
 yay -S visual-studio-code-bin
 
-sudo pacman -S zsh
-
-# chsh -s /bin/zsh
-
-git clone https://github.com/H-goto16/zshrc.git
-
-cp zshrc/.zshrc ~/
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-
-# source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-sudo pacman -S zsh-syntax-highlighting
-# source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 yay -S nvm
 
 source /usr/share/nvm/init-nvm.sh
@@ -55,40 +40,11 @@ touch ~/.ssh/config
 echo "Host *
     IPQoS=0x00" >> ~/.ssh/config
 
-# oh-my-posh
-
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-sudo chmod +x /usr/local/bin/oh-my-posh
-mkdir ~/.poshthemes
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-chmod u+rw ~/.poshthemes/*.omp.*
-rm ~/.poshthemes/themes.zip
-
 git config --global user.email $gitEmail
 git config --global user.name $gitName
 
-#echo "Select 7 14 27 28"
-
-#sudo pacman -S gnome
-
-#touch ~/.xinitrc
-
-#echo "export XDG_SESSION_TYPE=x11
-#export GDK_BACKEND=x11
-#exec gnome-session" >> ~/.xinitrc
-
 sudo pacman -S fcitx5-im fcitx5-mozc
-
-#gsettings set org.gnome.desktop.interface enable-animations false
-
-#gsettings set org.gnome.desktop.interface color-scheme prefer-dark 
 
 sudo cp ~/ArchLinuxSetUp/environment /etc/environment
 
 cat ~/.ssh/id_rsa.pub
-
-# ssh -T git@github.com -vvv
-# zsh
-# source ~/.zshrc
-
