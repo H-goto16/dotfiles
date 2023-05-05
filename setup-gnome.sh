5035bfa1901134d818bc3846f4fb00e6b44b3617
h-goto16 <h.goto.engineer@gmail.com>
2023-05-06 07:57:57 +0900

    fix: xanmod

--- setup-gnome.sh
+++
@@ -1,7 +0,0 @@
-echo "Select 7 14 27 28"
-
-sudo pacman -S gnome
-
-gsettings set org.gnome.desktop.interface enable-animations false
-
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark 
