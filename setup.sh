76860118b1fadece7de8d3139a7b7f60c0986be8
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2023-06-15 14:22:30 +0900

    fix: delete comment out

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# echo -n "Input git Name: "
-# read gitName
-# echo -n "Input git Email: "
-# read gitEmail
+echo -n "Input git Name: "
+read gitName
+echo -n "Input git Email: "
+read gitEmail
 
 sudo pacman -Syu --needed - < pkglist/base.txt --noconfirm
 
@@ -14,20 +14,20 @@ makepkg -si --noconfirm
 yay -S - < "$(dirname "${BASH_SOURCE[0]}")/pkglist/app.txt" --noconfirm
 
 
-# mkdir -p ~/.ssh && cd ~/.ssh
+mkdir -p ~/.ssh && cd ~/.ssh
 
-# ssh-keygen -t rsa
+ssh-keygen -t rsa
 
-# touch ~/.ssh/config
+touch ~/.ssh/config
 
-# echo "Host *
-#     IPQoS=0x00" >> ~/.ssh/config
+echo "Host *
+    IPQoS=0x00" >> ~/.ssh/config
 
-# git config --global user.email "$gitEmail"
-# git config --global user.name "$gitName"
+git config --global user.email "$gitEmail"
+git config --global user.name "$gitName"
 
-# sudo cp ~/dotfiles/fcitx/environment /etc/environment
+sudo cp ~/dotfiles/fcitx/environment /etc/environment
 
-# cp /etc/X11/xinit/xserverrc ~/.xserverrc
+cp /etc/X11/xinit/xserverrc ~/.xserverrc
 
-# cat ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub
