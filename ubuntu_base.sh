ca8a22d90304b0349ff071389c9c781e2fef74d6
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2023-12-15 09:15:40 +0000

    Create ubuntu_base.sh

---
+++ ubuntu_base.sh
@@ -0,0 +1,64 @@
+read -p "Enter your git email: " gitEmail
+read -p "Enter your git name: " gitName
+
+if [ -z "$gitEmail" ] || [ -z "$gitName" ]; then
+    gitEmail="h.goto.engineer@gmail.com"
+    gitName="H-goto16"
+fi
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install -y flatpak \
+zsh \
+git \
+vim \
+terminator \
+curl \
+software-properties-common \
+apt-transport-https \
+ca-certificates \
+apt-transport-https
+
+git config --global user.email "$gitEmail"
+git config --global user.name "$gitName"
+
+# install vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# install google-chrome
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg > /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+sudo apt update -y
+sudo apt install code google-chrome-stable -y
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+
+nvm install stable
+npm i -g yarn
+
+# setup zsh
+
+# auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# auto complete
+mkdir ~/Repos
+cd ~/Repos
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+
+# syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
