c8bcb6befe43d4f361436ed2542d92393d19cb28
H-goto16 <h.goto.engineer@gmail.com>
2024-09-02 17:04:31 +0900

    feat: update terminal settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,12 +8,17 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.font = wezterm.font 'YaHei Consolas Hybrid'
 config.font_size = 15
-config.scrollback_lines = 5000
+config.scrollback_lines = 50000
 config.use_ime = true
 config.exit_behavior = 'CloseOnCleanExit'
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE" 
-
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 wezterm.on('gui-startup', function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
