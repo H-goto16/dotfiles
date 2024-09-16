7bee3a0854f87752408669748d55c8e90f4e37c5
H-goto16 <h.goto.engineer@gmail.com>
2024-09-16 13:22:05 +0900

    feat: update

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,20 +4,18 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 -- coc --
 Plug ('neoclide/coc.nvim', {['branch'] = 'release'})
+-- nvim treesitter --
+Plug ('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 -- color scheme --
 Plug ('tomasiser/vim-code-dark')
 Plug ('Mofiqul/vscode.nvim')
 Plug ("LunarVim/darkplus.nvim")
 -- JSX Plugin --
-Plug ('leafgarland/typescript-vim')
 Plug ('peitalin/vim-jsx-typescript')
 Plug ('pangloss/vim-javascript')
-Plug ('leafgarland/typescript-vim')
 Plug ('maxmellon/vim-jsx-pretty')
 Plug ('jparise/vim-graphql')
 Plug ('leafgarland/typescript-vim')
--- treesitter --
-Plug ('nvim-treesitter/nvim-treesitter')
 -- material icon theme --
 Plug ('Allianaab2m/nvim-material-icon-v3')
 -- status bar --
@@ -48,9 +46,11 @@ Plug ('numToStr/Comment.nvim')
 Plug ('cohama/lexima.vim')
 -- github copilot --
 Plug ('github/copilot.vim')
+-- smooth scroll --
+Plug ('karb94/neoscroll.nvim')
 vim.call('plug#end')
 
-vim.cmd("colorscheme codedark")
+vim.cmd("colorscheme vscode")
 -- vim settings --
 
 vim.cmd('set number')
@@ -65,32 +65,58 @@ vim.cmd('set title')
 vim.cmd('set showmatch')
 vim.cmd('set clipboard+=unnamed')
 vim.cmd('set tags=tags;/')
+vim.cmd(":command Sd CocCommand tsserver.goToSourceDefinition")
+vim.cmd('nnoremap <C-LeftMouse> :Sd<CR> ')
 -- nvim tree
 require'nvim-tree'.setup {
 	view = {
 		side = 'right'
 	}
 }
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
 -- enable plugins
-require'error-lens'.setup{}
-require'ibl'.setup{}
-require'lualine'.setup{}
-require"scrollbar".setup{}
+require('error-lens').setup{}
+require('ibl').setup{}
+require('lualine').setup{}
+require("scrollbar").setup{}
 require('gitsigns').setup()
 require("scrollbar.handlers.gitsigns").setup()
 require('Comment').setup()
-
+require('neoscroll').setup({
+  mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
+    '<C-u>', '<C-d>',
+    '<C-b>', '<C-f>',
+    '<C-y>', '<C-e>',
+    'zt', 'zz', 'zb',
+  },
+  hide_cursor = true,          -- Hide cursor while scrolling
+  stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+  respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+  cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+  easing = 'linear',           -- Default easing function
+  pre_hook = nil,              -- Function to run before the scrolling animation starts
+  post_hook = nil,             -- Function to run after the scrolling animation ends
+  performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+  ignored_events = {           -- Events ignored while scrolling
+      'WinScrolled', 'CursorMoved'
+  },
+})
+neoscroll = require('neoscroll')
 vim.g.coc_user_config = {
   ["coc.preferences.enableFloatHighlight"] = true,
   ["coc.preferences.jumpCommand"] = "edit",
   ["coc.preferences.jumpKey"] = "<C-LeftMouse>"
 }
--- vim.cmd("autocmd BufNewFile,BufRead *.tsx,*.jsx set filetype=typescriptreact")
--- vim.cmd("hi tsxTagName guifg=#E06C75")
--- vim.cmd("hi tsxComponentName guifg=#E06C75")
--- vim.cmd("hi tsxCloseComponentName guifg=#E06C75")
--- vim.cmd("hi tsxTagName guifg=#E06C75")
--- vim.cmd("hi tsxComponentName guifg=#E06C75")
---vim.cmd("hi tsxCloseComponentName guifg=#E06C75")
+
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true })
+
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {"typescript", "javascript", "tsx", "json", "markdown"},
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true
+  }
+}
