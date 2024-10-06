e710e5479f84059661abbfe346d03879dcbcc171
H-goto16 <h.goto.engineer@gmail.com>
2024-10-06 13:21:29 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
--- Vim API Shortcuts
 local Plug = vim.fn['plug#']
 
 -- Plugin Installation
@@ -6,149 +5,174 @@ vim.call('plug#begin')
 
 -- Plugin Declarations
 local plugins = {
-  -- coc.nvim
-  { 'neoclide/coc.nvim', { branch = 'release' } },
-  -- nvim-treesitter
-  { 'nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' } },
-  -- Color schemes
-  'tomasiser/vim-code-dark',
-  'Mofiqul/vscode.nvim',
-  'LunarVim/darkplus.nvim',
-  -- JSX/TSX
-  'peitalin/vim-jsx-typescript',
-  'pangloss/vim-javascript',
-  'maxmellon/vim-jsx-pretty',
-  'jparise/vim-graphql',
-  'leafgarland/typescript-vim',
-  -- Material icons
-  'Allianaab2m/nvim-material-icon-v3',
-  -- Status/tab bars
-  'nvim-lualine/lualine.nvim',
-  'nvim-tree/nvim-web-devicons',
-  'lewis6991/gitsigns.nvim',
-  'akinsho/bufferline.nvim',
-  -- Sidebar plugins
-  'nvim-tree/nvim-tree.lua',
-  'kdheepak/lazygit.nvim',
-  -- LSP/Eslint
-  'neovim/nvim-lspconfig',
-  'jose-elias-alvarez/null-ls.nvim',
-  'MunifTanjim/eslint.nvim',
-  -- Error Lens
-  'chikko80/error-lens.nvim',
-  'nvim-telescope/telescope.nvim',
-  -- Scrollbar
-  'petertriho/nvim-scrollbar',
-  -- Indentation/Rainbow
-  'lukas-reineke/indent-blankline.nvim',
-  'TheGLander/indent-rainbowline.nvim',
-  -- Commenting/Auto-brackets
-  'numToStr/Comment.nvim',
-  'cohama/lexima.vim',
-  -- GitHub Copilot
-  'github/copilot.vim',
-  -- Smooth scrolling
-  'karb94/neoscroll.nvim',
-  -- Bracket colorization
-  'HiPhish/rainbow-delimiters.nvim',
-  'HiPhish/nvim-ts-rainbow2',
-  -- Notifications
-  'rcarriga/nvim-notify',
-  -- search and replace
-  'nvim-lua/plenary.nvim',
-  'nvim-pack/nvim-spectre'
+        -- coc.nvim
+        { 'neoclide/coc.nvim',               { branch = 'release' } },
+        -- nvim-treesitter
+        { 'nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' } },
+        -- Color schemes
+        'tomasiser/vim-code-dark',
+        'Mofiqul/vscode.nvim',
+        'LunarVim/darkplus.nvim',
+        -- JSX/TSX support
+        'peitalin/vim-jsx-typescript',
+        'pangloss/vim-javascript',
+        'maxmellon/vim-jsx-pretty',
+        'jparise/vim-graphql',
+        'leafgarland/typescript-vim',
+        -- Material icons
+        'Allianaab2m/nvim-material-icon-v3',
+        -- Status/tab bars
+        'nvim-lualine/lualine.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'lewis6991/gitsigns.nvim',
+        'akinsho/bufferline.nvim',
+        -- Sidebar plugins
+        'nvim-tree/nvim-tree.lua',
+        'kdheepak/lazygit.nvim',
+        -- LSP/Eslint
+        'neovim/nvim-lspconfig',
+        'jose-elias-alvarez/null-ls.nvim',
+        'MunifTanjim/eslint.nvim',
+        -- Error Lens
+        'chikko80/error-lens.nvim',
+        'nvim-telescope/telescope.nvim',
+        -- Scrollbar
+        'petertriho/nvim-scrollbar',
+        -- Indentation/Rainbow
+        'lukas-reineke/indent-blankline.nvim',
+        'TheGLander/indent-rainbowline.nvim',
+        -- Commenting/Auto-brackets
+        'numToStr/Comment.nvim',
+        'cohama/lexima.vim',
+        -- GitHub Copilot
+        'github/copilot.vim',
+        -- Smooth scrolling
+        'karb94/neoscroll.nvim',
+        -- Bracket colorization
+        'HiPhish/rainbow-delimiters.nvim',
+        'HiPhish/nvim-ts-rainbow2',
+        -- Notifications
+        'rcarriga/nvim-notify',
+        -- Search and replace
+        'nvim-lua/plenary.nvim',
+        'nvim-pack/nvim-spectre'
 }
 
 -- Register plugins
 for _, plug in ipairs(plugins) do
-  if type(plug) == "table" then
-    Plug(plug[1], plug[2])
-  else
-    Plug(plug)
-  end
+        if type(plug) == "table" then
+                Plug(plug[1], plug[2])
+        else
+                Plug(plug)
+        end
 end
 
 vim.call('plug#end')
 
--- Colorscheme
-vim.cmd('colorscheme vscode')
-
--- General Vim Settings
-local settings = {
-  'set number', 'set cursorline', 'set expandtab',
-  'set hlsearch', 'set ignorecase', 'set incsearch',
-  'set smartcase', 'set autoindent', 'set title',
-  'set showmatch', 'set clipboard+=unnamed',
-  'set tags=tags;/', 'set cmdheight=0',
-  'set list', 'set lcs+=space:·', 'set listchars+=tab:»\\ ', 'set listchars+=trail:·', 'set listchars+=eol:↲', 'set listchars+=nbsp:␣',
-}
-
-for _, setting in ipairs(settings) do
-  vim.cmd(setting)
+-- Helper functions for settings and mappings
+local function set_vim_options(options)
+        for _, opt in ipairs(options) do
+                vim.cmd(opt)
+        end
 end
 
--- Commands & Key Mappings
-vim.cmd(":command Sd CocCommand tsserver.goToSourceDefinition")
-vim.cmd("autocmd TermOpen * setlocal nonumber")
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*.tsx", "*.ts", "*.js", "*.jsx" },
-  command = "CocCommand tsserver.executeAutofix"
-})
+local function set_keymaps(mappings)
+        for _, map in ipairs(mappings) do
+                vim.api.nvim_set_keymap(map.mode, map.key, map.action, map.opts)
+        end
+end
+
+-- General Vim Settings
+local general_settings = {
+        'set number', 'set cursorline', 'set expandtab',
+        'set hlsearch', 'set ignorecase', 'set incsearch',
+        'set smartcase', 'set autoindent', 'set title',
+        'set showmatch', 'set clipboard+=unnamed',
+        'set tags=tags;/', 'set cmdheight=0',
+        'set list', 'set lcs+=space:·', 'set listchars+=tab:»\\ ',
+        'set listchars+=trail:·', 'set listchars+=eol:↲',
+        'set listchars+=nbsp:␣',
+}
+set_vim_options(general_settings)
+
 vim.opt.termguicolors = true
 
 -- Key Mappings
 local keymaps = {
-  { mode = 'n', key = '<C-s>', action = ':w<CR>', opts = { noremap = true, silent = true } },
-  { mode = 'i', key = '<C-s>', action = '<Esc>:w<CR>a', opts = { noremap = true, silent = true } },
-  { mode = 'v', key = '<C-s>', action = '<Esc>:w<CR>', opts = { noremap = true, silent = true } },
+        { mode = 'n', key = '<C-s>', action = ':w<CR>',       opts = { noremap = true, silent = true } },
+        { mode = 'i', key = '<C-s>', action = '<Esc>:w<CR>a', opts = { noremap = true, silent = true } },
+        { mode = 'v', key = '<C-s>', action = '<Esc>:w<CR>',  opts = { noremap = true, silent = true } },
 }
+set_keymaps(keymaps)
 
-vim.api.nvim_set_keymap('n', '<A-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-
-
-for _, map in ipairs(keymaps) do
-  vim.api.nvim_set_keymap(map.mode, map.key, map.action, map.opts)
-end
+-- Alt arrow key mappings for moving lines
+set_keymaps({
+        { mode = 'n', key = '<A-Up>',   action = ':m .-2<CR>==',     opts = { noremap = true, silent = true } },
+        { mode = 'n', key = '<A-Down>', action = ':m .+1<CR>==',     opts = { noremap = true, silent = true } },
+        { mode = 'v', key = '<A-Up>',   action = ":m '<-2<CR>gv=gv", opts = { noremap = true, silent = true } },
+        { mode = 'v', key = '<A-Down>', action = ":m '>+1<CR>gv=gv", opts = { noremap = true, silent = true } },
+})
 
 -- Plugin Setup
-require('nvim-tree').setup { view = { side = 'right' } }
-require('error-lens').setup{}
-require('lualine').setup{}
-require('gitsigns').setup{}
-require('scrollbar').setup{}
-require('scrollbar.handlers.gitsigns').setup()
-require('nvim-web-devicons').get_icons()
-require('Comment').setup()
-require("ibl").setup(require("indent-rainbowline").make_opts({}))
-require('neoscroll').setup {
-  mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-  hide_cursor = false,
-  stop_eof = true,
-  respect_scrolloff = false,
-  cursor_scrolls_alone = true,
-  easing = 'linear',
-  performance_mode = false,
-  ignored_events = { 'WinScrolled', 'CursorMoved' }
+local plugin_setup = {
+        ['nvim-tree'] = function() require('nvim-tree').setup { view = { side = 'right' } } end,
+        ['error-lens'] = function() require('error-lens').setup {} end,
+        ['lualine'] = function() require('lualine').setup {} end,
+        ['gitsigns'] = function() require('gitsigns').setup {} end,
+        ['scrollbar'] = function()
+                require('scrollbar').setup {}
+                require('scrollbar.handlers.gitsigns').setup()
+        end,
+        ['nvim-web-devicons'] = function() require('nvim-web-devicons').get_icons() end,
+        ['Comment'] = function() require('Comment').setup() end,
+        ['bufferline'] = function()
+                require('bufferline').setup {
+                        options = {
+                                numbers = 'ordinal',
+                                diagnostics = 'coc',
+                                diagonostic_indicator = function(count)
+                                        return '(' .. count .. ')'
+                                end
+                        }
+                }
+        end,
+        ['nvim-treesitter'] = function()
+                require('nvim-treesitter.configs').setup {
+                        ensure_installed = { "typescript", "javascript", "json", "markdown" },
+                        highlight = { enable = true },
+                        rainbow = {
+                                enable = true,
+                                extended_mode = true,
+                        },
+                }
+        end,
+        ['spectre'] = function() require('spectre').setup() end,
+        ['ibl'] = function()
+                require("ibl").setup(require("indent-rainbowline").make_opts({}))
+        end
 }
-require('bufferline').setup{}
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "typescript", "javascript", "json", "markdown" },
-  sync_install = false,
-  auto_install = true,
-  highlight = { enable = true },
-  rainbow = {
-        enable = true,
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  },
-}
-require('spectre').setup()
+for _, setup in pairs(plugin_setup) do
+        setup()
+end
 
-vim.g.coc_user_config = {
-  ["coc.preferences.enableFloatHighlight"] = true,
-  ["coc.preferences.jumpCommand"] = "edit",
-  ["coc.preferences.jumpKey"] = "<C-LeftMouse>"
+-- Commands & Autocommands
+vim.cmd("autocmd TermOpen * setlocal nonumber")
+vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = { "*.tsx", "*.ts", "*.js", "*.jsx" },
+        command = "CocCommand tsserver.executeAutofix"
+})
+
+vim.cmd('colorscheme vscode')
+
+-- Key mappings for CoCList and other coc.nvim features
+local coc_keymaps = {
+        { mode = "n", key = "<space>a", action = ":<C-u>CocList diagnostics<cr>", opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>e", action = ":<C-u>CocList extensions<cr>",  opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>c", action = ":<C-u>CocList commands<cr>",    opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>o", action = ":<C-u>CocList outline<cr>",     opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>s", action = ":<C-u>CocList -I symbols<cr>",  opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>j", action = ":<C-u>CocNext<cr>",             opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>k", action = ":<C-u>CocPrev<cr>",             opts = { silent = true, nowait = true } },
+        { mode = "n", key = "<space>p", action = ":<C-u>CocListResume<cr>",       opts = { silent = true, nowait = true } },
 }
+set_keymaps(coc_keymaps)
